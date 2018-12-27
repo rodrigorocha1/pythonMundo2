7968c58624d8c5d20c6201eba5e8cee6d6284dba
rrrocha <rodrigo.rocha25@fatec.sp.gov.br>
2018-12-27 17:24:37 -0200

    Exercício 36

--- exc36.py
+++ exc36.py
@@ -8,5 +8,13 @@ valorCasa = float(input('Qual o valor da casa R$? '))
 salario = float(input('Qual o salário do comprador R$? '))
 totalAnos = int(input('Quantos anos ele vai pagar a casa? '))
 
+prestacao = valorCasa / (totalAnos * 12) #Valor da casa dividido pelo tempo convertido em meses = prestação
+
+if prestacao >= (salario * 0.30):
+    print(f'Valor da Casa: R$ {valorCasa:.2f}\nSalário: R$ {salario:.2f}\nPrestação: R$ {prestacao:.2f}\nTotal de anos: {totalAnos}\nResultado: Emprestimo negado')
+else:
+    print(
+        f'Valor da Casa: R$ {valorCasa:.2f}\nSalário: R$ {salario:.2f}\nPrestação: R$ {prestacao:.2f}\nTotal de anos: {totalAnos}\nResultado: Emprestimo aceito')
+
 
 
