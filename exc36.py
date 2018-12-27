9d0c2e0929f2b51971a3bee2953e648f47f04ab7
rrrochaa <silva.rodrigo31@outlook.com>
2018-12-27 17:11:49 -0200

    Exercício 36

---
+++ exc36.py
@@ -0,0 +1,12 @@
+'''Exercício Python 036: Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa.
+Pergunte o valor da casa, o salário do comprador e em quantos anos ele vai pagar.
+ A prestação mensal não pode exceder 30% do salário ou então o empréstimo será negado. '''
+
+print('*' * 10, 'Emprestimo', '*' * 10)
+
+valorCasa = float(input('Qual o valor da casa R$? '))
+salario = float(input('Qual o salário do comprador R$? '))
+totalAnos = int(input('Quantos anos ele vai pagar a casa? '))
+
+
+
