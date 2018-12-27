40d1dabb6f62d1abeb63b6cffe0b316a26f806d3
rrrochaa <silva.rodrigo31@outlook.com>
2018-12-27 17:31:46 -0200

    Exercício 36

--- exc36.py
+++ exc36.py
@@ -8,7 +8,8 @@ valorCasa = float(input('Qual o valor da casa R$? '))
 salario = float(input('Qual o salário do comprador R$? '))
 totalAnos = int(input('Quantos anos ele vai pagar a casa? '))
 
-prestacao = valorCasa / (totalAnos * 12) #Valor da casa dividido pelo tempo convertido em meses = prestação
+# Valor da casa dividido pelo tempo convertido em meses = prestação
+prestacao = valorCasa / (totalAnos * 12)
 
 if prestacao >= (salario * 0.30):
     print(f'Valor da Casa: R$ {valorCasa:.2f}\n'
