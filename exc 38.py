f3d8a320ec6118555fd919a016574e96c556fab8
rrrochaa <silva.rodrigo31@outlook.com>
2019-01-03 14:56:37 -0200

    Exercício Python 038: Escreva um programa que leia dois números inteiros e compare-os. mostrando na tela uma mensagem: - O primeiro valor é maior - O segundo valor é maior - Não existe valor maior, os dois são iguais

---
+++ exc 38.py
@@ -0,0 +1,16 @@
+
+print('*' * 20, 'Maior e menor entre três números', '*' * 20)
+
+num1 = int(input('Digite um número: '))
+num2 = int(input('Digite um número: '))
+
+
+if num1 > num2:
+    print(f'{num1} é o maior ')
+elif num2 > num1:
+    print(f'{num2} é o maior ')
+else:
+    print(f'Os números são iguais')
+
+
+
