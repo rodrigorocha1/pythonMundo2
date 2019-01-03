e774dce1991de7467970cc4cc87dbfacec3c5d04
rrrochaa <silva.rodrigo31@outlook.com>
2019-01-03 16:20:08 -0200

    Exercício Python 042: Refaça o DESAFIO 035 dos triângulos, acrescentando o recurso de mostrar que tipo de triângulo será formado: - EQUILÁTERO: todos os lados iguais - ISÓSCELES: dois lados iguais, um diferente - ESCALENO: todos os lados diferentes

---
+++ exc42.py
@@ -0,0 +1,15 @@
+print('*' * 20, 'Classificação do triângulo', '*' * 20)
+
+l1 = float(input('Digite o lado do triângulo: '))
+l2 = float(input('Digite o lado do triângulo: '))
+l3 = float(input('Digite o lado do triângulo: '))
+
+if (l1 < l2 + l3) and (l2 < l1 + l3) and (l3 < l1 + l2):
+    if l1 == l2 == l3:
+        print(f'Os lados {l1}, {l2}, {l3} formam um triângulo equilatero')
+    elif l1 != l2 != l3 != l1:
+        print(f'Os lados {l1}, {l2}, {l3} formam um triângulo escaleno ')
+    else:
+        print(f'Os lados {l1}, {l2}, {l3} formam um triângulo isórceles')
+else:
+    print(f'Os lados {l1}, {l2}, {l3} não podem formar um triângulo')
