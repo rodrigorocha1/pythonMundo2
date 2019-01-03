491173871f9b6a208da4ea53b7be18c67bcc2446
rrrochaa <silva.rodrigo31@outlook.com>
2019-01-03 16:32:50 -0200

    Exercício Python 043: Desenvolva uma lógica que leia o peso e a altura de uma pessoa, calcule seu Índice de Massa Corporal (IMC) e mostre seu status, de acordo com a tabela abaixo: - IMC abaixo de 18,5: Abaixo do Peso - Entre 18,5 e 25: Peso Ideal - 25 até 30: Sobrepeso - 30 até 40: Obesidade - Acima de 40: Obesidade Mórbida

--- exc43.py
+++ exc43.py
@@ -1,10 +1,3 @@
-'''Exercício Python 043: Desenvolva uma lógica que leia o peso e a altura de uma pessoa,
-calcule seu Índice de Massa Corporal (IMC) e mostre seu status, de acordo com a tabela abaixo:
-- IMC abaixo de 18,5: Abaixo do Peso
-- Entre 18,5 e 25: Peso Ideal
-- 25 até 30: Sobrepeso - 30 até 40: Obesidade
- - Acima de 40: Obesidade Mórbida '''
-
 from math import pow
 
 print('*' * 20, 'IMC', '*' * 20)
@@ -13,5 +6,19 @@ peso = float(input('Digite o peso: '))
 altura = float(input('Digita a altura: '))
 
 IMC = peso / pow(altura, 2)
+
+if IMC < 18.5:
+    print('Abaixo do peso')
+elif 18.5 <= IMC <= 25.0:
+    print('Peso ideal')
+elif 25.0 < IMC <= 30.0:
+    print("Sobrepeso")
+elif 30 < IMC <= 40:
+    print('Obesidade')
+else:
+    print('Obesidade Mórbida')
+
+
+
 
 
